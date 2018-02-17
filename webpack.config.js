8f0cb6db03c0860c2f6b8a95fd134feba1a9808c
Nitin Parasa <nitin.parasa3@gmail.com>
2018-02-17 23:07:16 +0100

    Changes to webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,52 @@
 // entry
 // where to output the final bundled file
 const path = require('path');
-console.log(__dirname);
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname,'public'), // this should be an absolute path as this key cant contain "/".
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-           test: /\.s?css$/,
-           use: [
-               'style-loader',
-               'css-loader',
-               'sass-loader'
-           ],
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname,'public'),
-        historyApiFallback: true
-    }
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    CSSExtract = new ExtractTextPlugin('app.css');
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname,'public'), // this should be an absolute path as this key cant contain "/".
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, 
+            {
+               test: /\.s?css$/,
+               use: CSSExtract.extract({
+                use:[
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname,'public'),
+            historyApiFallback: true
+        }
+    };
 };
 
 // loader - tells webpack what to do with a set of complex code
