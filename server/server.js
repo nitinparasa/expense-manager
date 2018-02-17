616aadc9f71ccd39afb0ca63103e0fd79d87fc4a
Nitin Parasa <nitin.parasa3@gmail.com>
2018-02-17 23:47:15 +0100

    Production build and server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// App Configuration
+// registering the middleware
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log('App Initialised');
+});
