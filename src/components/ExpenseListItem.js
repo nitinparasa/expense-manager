9dd211f8648e54c4297f71c998f1144e747c0fd8
Nitin Parasa <nitin.parasa3@gmail.com>
2018-02-18 09:11:26 +0100

    Formatting currency for amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
         <div>
             <Link to={`/edit/${id}`}> <p> {description}</p></Link>
-            <p> {amount} - {createdAt}</p>
+            <p> {numeral(amount).format('$0,0.00')} - {moment(createdAt).format('MMMM Do YYYY')}</p>
         </div>
 );
 
